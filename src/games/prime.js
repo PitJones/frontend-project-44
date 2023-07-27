1b084c397880bd55a4f8337c3d531843b0993bb8
s.prusakov <scubb@bk.ru>
2023-07-27 14:43:58 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,6 +3,9 @@ import { randomNumber, generalGameLogic } from '../index.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number === 1) {
+    return false;
+  }
   for (let divisor = 2; divisor < number; divisor += 1) {
     if (number % divisor === 0) {
       return false;
