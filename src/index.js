d1e61c3b7c4711eb2af2cab00135e3d6d5aac90f
s.prusakov <scubb@bk.ru>
2023-07-29 12:00:41 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import readlineSync from 'readline-sync';
 
-const greetingUser = () => {
+const runGreetingUser = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 };
 
-const name = greetingUser();
-
-const randomNumber = () => {
+const generateRandomNumber = () => {
   const number = Math.round(Math.random() * 100);
   return number;
 };
 
-const randomNumberInRange = (min, max) => Math.round(Math.random() * (max - min) + min);
+const generateRandomNumberInRange = (min, max) => Math.round(Math.random() * (max - min) + min);
 
 const numberOfRounds = 3;
-const generalGameLogic = (gameDescription, gameLogic) => {
+const runGeneralGameLogic = (gameDescription, runGameLogic) => {
+  const name = runGreetingUser();
   console.log(gameDescription);
   for (let i = 0; i < numberOfRounds; i += 1) {
-    const [rightAnswer, question] = gameLogic();
+    const [rightAnswer, question] = runGameLogic();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === rightAnswer) {
@@ -29,7 +28,9 @@ const generalGameLogic = (gameDescription, gameLogic) => {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${name}!`);
       return;
     }
-  } console.log(`Congratulations, ${name}!`);
+  }
+
+  console.log(`Congratulations, ${name}!`);
 };
 
-export { randomNumberInRange, randomNumber, generalGameLogic };
+export { generateRandomNumberInRange, generateRandomNumber, runGeneralGameLogic };
